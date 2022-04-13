de11768d92b95e1ebf4f92c06c0573dfa49fb0b1
jaysonvandam <jaysonvandam@gmail.com>
2022-04-13 17:21:38 -0400

    started work on html parser

--- web_scraping.py
+++ web_scraping.py
@@ -1,2 +1,13 @@
 # webscraping.py -- script that goes out and gets the speedrun times from speedrun.com
 # Based on TechWithTim's tutorial at https://www.youtube.com/watch?v=gRLHr664tXA
+
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.speedrun.com/sm64'
+result = requests.get(url)
+print(result.text)
+#html parser does NOT work on speedrun.com unfortunately, so I will need to get the
+#info using some other method.
+#doc = BeautifulSoup(result, "html.parser")
+#print(doc.prettify())
